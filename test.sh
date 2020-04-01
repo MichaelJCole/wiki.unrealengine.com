8b18ce92773781dbe3694752a167bf5ea806b83e
Michael Cole <gitlab@michaelcole.com>
2020-04-01 17:01:41 -0500

    Clean up

--- test.sh
+++
@@ -1,4 +0,0 @@
-mv docs/.vuepress/dist docs/.vuepress/wiki.unrealengine.com  
-sensible-browser http://localhost/wiki.unrealengine.com
-http-server docs/.vuepress 
-mv docs/.vuepress/wiki.unrealengine.com docs/.vuepress/dist
