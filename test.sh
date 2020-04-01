ffdb121a3558079c88950d2359a002eb90fadc12
Michael Cole <gitlab@michaelcole.com>
2020-04-01 17:17:00 -0500

    Turn prefetch off

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 mv docs/.vuepress/dist docs/.vuepress/wiki.unrealengine.com  
-sensible-browser http://localhost/wiki.unrealengine.com
+sensible-browser http://localhost:8080/wiki.unrealengine.com
 http-server docs/.vuepress 
 mv docs/.vuepress/wiki.unrealengine.com docs/.vuepress/dist
