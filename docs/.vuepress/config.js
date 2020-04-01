86a7e25aee17df3d8a8101f3a1e6032d0dc00d98
Michael Cole <gitlab@michaelcole.com>
2020-04-01 12:56:51 -0500

    Update home page with index

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,14 @@
+const glob = require('glob');
+
+let markdownFiles = glob.sync('docs/**/*.md').map(f => '/' + f); 
+// update the docs/**/*.md pattern with your folder structure 
+
 module.exports = {
   title: 'wiki.unrealengine.com',
   description: 'A static site pulled from the internet archive',
   base: '/wiki.unrealengine.com/',
   theme: '@vuepress/theme-default',
   themeConfig: {
-    displayAllHeaders: true // Default: false
+    sidebar: markdownFiles
   }
 }
