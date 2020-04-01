03d003123e89bdb03396fc2290c2e01e9f40e1a4
Michael Cole <gitlab@michaelcole.com>
2020-04-01 11:57:42 -0500

    Travis you are such a disappointment.

--- deploy_github_pages.sh
+++ deploy_github_pages.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:MichaelJCole/wiki.unrealengine.com.git master:gh-pages
 
 cd -
