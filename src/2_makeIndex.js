97790767e2283e65a31bcaa33fbcdb6facec5ecd
Michael Cole <gitlab@michaelcole.com>
2020-04-02 10:37:53 -0500

    Tweaks

--- src/2_makeIndex.js
+++ src/2_makeIndex.js
@@ -28,7 +28,7 @@ mdFiles = recFindByExt('../docs','md')
 var readmeContent = fs.readFileSync('../README.md', 'UTF-8');
 
 fileList = mdFiles.reduce((total, item) => {
-  var name = item.replace('../docs/', '').replace('/README.md', '').trim()
+  var name = item.replace('../docs/', '').replace('_', ' ').replace('/README.md', '').trim()
   var path = item.replace('../docs', '')
   total += `- [${name}](${path})\n\n`
   return total
